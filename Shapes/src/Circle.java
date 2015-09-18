f97831b4207988f2bbf056fefc02e97ce067cfe0
Thomas Cotroneo <tcotroneo@drew.edu>
2015-09-18 09:16:04 -0400

    added getArea and getSideLength

--- Shapes/src/Circle.java
+++ Shapes/src/Circle.java
@@ -19,10 +19,20 @@ private int radius;
   public double getPerimeter(){
 	  return 2* radius * 3.14;
   }
+  
+  public double getArea(){
+	  return radius * radius * 3.14;
+  }
+  
+  public double getSideLength(double sideLength){
+	  return sideLength;
+  }
+  
   public static void main(String [] args){
 	  Circle c = new Circle();
 	  System.out.println(c.getWidth());
 	  System.out.println(c.getHeight());
 	  System.out.println(c.getPerimeter());
+	  System.out.println(c.getArea());
   }
 }
